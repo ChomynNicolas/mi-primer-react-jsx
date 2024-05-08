02f2743359b18171e583220ee661ea57d62503b6
ChomynNicolas <chomynnicolas2@gmail.com>
2024-05-07 23:34:18 -0400

    mi primera pagina con react y jsx

---
+++ src/Components/ListComponent/ListComponent.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class ListComponent extends Component {
+  render() {
+    return <div>
+      <h3>Cosas que tengo que hacer:</h3>
+      <ul>
+        <li>Aprender React</li>
+        <li>Escalar el Everest</li>
+        <li>Correr una marathon</li>
+        <li>Alimentar a los perros</li>
+      </ul>
+
+    </div>;
+  }
+}
